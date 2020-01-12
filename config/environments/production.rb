d3d62879f7f46b44af5fe2b7b0c7066af25c4ab6
shengliang <ahliang51@hotmail.com>
2020-01-12 21:04:12 +0800

    edited environment.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   config.serve_static_assets = true
+  config.assets.enabled = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
